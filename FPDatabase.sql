41cbfab61c5705241f787b73db9b5ceca607398a
Ecramer7 <ecramer7@gmail.com>
2023-01-09 21:28:10 -0700

    ETL to prepare csv for SQL

---
+++ FPDatabase.sql
@@ -0,0 +1,13 @@
+-- Creating tables
+-- Meteorites
+CREATE TABLE Meteorite_info (
+	mass_g INT NOT NULL,
+    impact_year INT NOT NULL,
+	meteorite_name VARCHAR(100) NOT NULL,
+	recclass VARCHAR(40),
+	PRIMARY KEY (name)
+);
+
+CREATE TABLE Meteorite_location (
+	 INT NOT NULL,
+);
