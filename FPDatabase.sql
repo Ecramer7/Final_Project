4f69bef7ae6c0089f038dd1cf82d9e6fcdd71139
Ecramer7 <ecramer7@gmail.com>
2023-01-09 21:48:54 -0700

    Made tables - need to connect data

--- FPDatabase.sql
+++ FPDatabase.sql
@@ -4,10 +4,15 @@ CREATE TABLE Meteorite_info (
 	mass_g INT NOT NULL,
     impact_year INT NOT NULL,
 	meteorite_name VARCHAR(100) NOT NULL,
-	recclass VARCHAR(40),
-	PRIMARY KEY (name)
+	recclass VARCHAR(100),
+	PRIMARY KEY (meteorite_name)
 );
 
 CREATE TABLE Meteorite_location (
-	 INT NOT NULL,
+	meteorite_name VARCHAR(100) NOT NULL,
+	retclat  INT NOT NULL,
+	reclong  INT NOT NULL,
+	PRIMARY KEY (meteorite_name)
 );
+
+
