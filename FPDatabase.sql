91a6408115a18ca622920bba22091c2f294d8411
Ecramer7 <ecramer7@gmail.com>
2023-01-11 19:56:58 -0700

    Final Database

--- FPDatabase.sql
+++ FPDatabase.sql
@@ -1,18 +1,16 @@
 -- Creating tables
 -- Meteorites
 CREATE TABLE Meteorite_info (
-	mass_g INT NOT NULL,
-    impact_year INT NOT NULL,
 	meteorite_name VARCHAR(100) NOT NULL,
 	recclass VARCHAR(100),
+	mass_g VARCHAR(100) NOT NULL,
+	impact_year INT NOT NULL,
 	PRIMARY KEY (meteorite_name)
 );
 
 CREATE TABLE Meteorite_location (
 	meteorite_name VARCHAR(100) NOT NULL,
-	retclat  INT NOT NULL,
-	reclong  INT NOT NULL,
+	retclat  VARCHAR(100) NOT NULL,
+	reclong  VARCHAR(100) NOT NULL,
 	PRIMARY KEY (meteorite_name)
 );
-
-
