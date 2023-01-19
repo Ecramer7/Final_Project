2f0f8266e033892b3f11f51c261dfa5eca5f2125
Ecramer7 <ecramer7@gmail.com>
2023-01-18 21:06:05 -0700

    Database updates

--- FPDatabase.sql
+++ FPDatabase.sql
@@ -3,14 +3,24 @@
 CREATE TABLE Meteorite_info (
 	meteorite_name VARCHAR(100) NOT NULL,
 	recclass VARCHAR(100),
-	mass_g VARCHAR(100) NOT NULL,
+	mass_g FLOAT8 NOT NULL,
 	impact_year INT NOT NULL,
 	PRIMARY KEY (meteorite_name)
 );
 
 CREATE TABLE Meteorite_location (
 	meteorite_name VARCHAR(100) NOT NULL,
-	retclat  VARCHAR(100) NOT NULL,
-	reclong  VARCHAR(100) NOT NULL,
+	retclat  FLOAT8 NOT NULL,
+	reclong  FLOAT8 NOT NULL,
+	PRIMARY KEY (meteorite_name)
+);
+
+CREATE TABLE Meteorite_final (
+	meteorite_name VARCHAR(100) NOT NULL,
+	recclass VARCHAR(100),
+	mass_g FLOAT8 NOT NULL,
+	impact_year INT NOT NULL,
+	retclat  FLOAT8 NOT NULL,
+	reclong  FLOAT8 NOT NULL,
 	PRIMARY KEY (meteorite_name)
 );
